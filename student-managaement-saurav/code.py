8ad61af518e902237ad32410479a9c1ee319eefc
singhsaurav1826 <singhsaurav1826@users.noreply.github.com>
2020-05-15 03:55:29 +0530

    Code file created.

---
+++ student-managaement-saurav/code.py
@@ -0,0 +1,59 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+class_1=['Geoffrey Hinton', 'Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2=['Hilary Mason','Carla Gentry','Corinna Cortes']
+
+# Concatenate both the strings
+new_class=class_1+class_2
+print(new_class)
+
+# Append the list
+new_class.append('Peter Warden')
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+courses={'Math':65,'English':70,'History':80,'French':70,'Science':60}
+print(courses)
+total=65+70+80+70+60
+print(total)
+percentage=(total/500)*100
+print(percentage)
+mathematics={'Geoffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
+topper=max(mathematics,key=mathematics.get)
+print(topper)
+topper='Andrew Ng'
+first_name=topper.split()[0]
+last_name=topper.split()[1]
+print(last_name)
+print(first_name)
+full_name=last_name+ ' '+first_name
+print(full_name)
+certificate_name=full_name.upper()
+print(certificate_name)
+
+
+
+
+# Create the Dictionary
+ 
+
+
+
+# Given string
+
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
